0438c8b17c8352d0b3c9792604adbb4d016db021
korzck <konstantin.koretskiy@gmail.com>
2023-02-28 21:22:22 +0300

    done second task

--- lab2/script.js
+++ lab2/script.js
@@ -1,1 +1,94 @@
+window.onload = function(){ 
 
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null
+    
+    outputElement = document.getElementById("result")
+    
+    digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+    
+    function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
+                a += digit
+            }
+            outputElement.innerHTML = a
+        } else {
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
+                b += digit
+                outputElement.innerHTML = b        
+            }
+        }
+    }
+    
+    digitButtons.forEach(button => {
+        button.onclick = function() {
+            const digitValue = button.innerHTML
+            onDigitButtonClicked(digitValue)
+        }
+    });
+    
+    document.getElementById("btn_op_mult").onclick = function() { 
+        if (a === '') return
+        selectedOperation = 'x'
+    }
+    document.getElementById("btn_op_plus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '+'
+    }
+    document.getElementById("btn_op_minus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '-'
+    }
+    document.getElementById("btn_op_div").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '/'
+    }
+    document.getElementById("btn_op_sign").onclick = function() { 
+        if (a === '') return
+        else if ( a !== '' && b === '') {
+            a = (+a)*(-1)
+            outputElement.innerHTML = a
+        }
+        else {
+            b = (+b)*(-1)
+            outputElement.innerHTML = b
+        }
+    }
+    
+    document.getElementById("btn_op_clear").onclick = function() { 
+        a = ''
+        b = ''
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
+    }
+    
+    document.getElementById("btn_op_equal").onclick = function() { 
+        if (a === '' || b === '' || !selectedOperation)
+            return
+            
+        switch(selectedOperation) { 
+            case 'x':
+                expressionResult = (+a) * (+b)
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;
+        }
+        
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+    
+        outputElement.innerHTML = a
+    }
+};
