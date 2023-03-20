c0b889af9afc773fe59a50b017f42dab0c5a5bc4
korzck <konstantin.koretskiy@gmail.com>
2023-03-20 19:59:50 +0300

    done step 8

--- lab3/components/product-card/index.js
+++ lab3/components/product-card/index.js
@@ -3,23 +3,33 @@ export class ProductCardComponent {
         this.parent = parent;
     }
 
-    getHTML() {
+    getHTML(data) {
         return (
             `
                 <div class="card" style="width: 300px;">
-                    <img class="card-img-top" src="https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg" alt="картинка">
+                    <img class="card-img-top" src="${data.src}" alt="картинка">
                     <div class="card-body">
-                        <h5 class="card-title">Акция</h5>
-                        <p class="card-text">Вот тут информация об акции</p>
-                        <button class="btn btn-primary"">Нажми на меня</button>
+                        <h5 class="card-title">${data.title}</h5>
+                        <p class="card-text">${data.text}</p>
+                        <button class="btn btn-primary"">Нажми</button>
                     </div>
                 </div>
             `
         )
     }
 
-    render() {
-        const html = this.getHTML();
+    getData() {
+        return {
+            id: 1,
+            src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
+            title: "Акция",
+            text: "У меня есть крутая акция"
+        }
+    }
+
+    render(data) {
+        data = this.getData()
+        const html = this.getHTML(data);
         this.parent.insertAdjacentHTML('beforeend', html);
     }
 }
