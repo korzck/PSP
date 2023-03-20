de6953b6d7e8cfda95a68bf8477ddcae0a25be9f
korzck <konstantin.koretskiy@gmail.com>
2023-03-20 22:20:20 +0300

    done lab 3

--- lab3/pages/product/index.js
+++ lab3/pages/product/index.js
@@ -1,4 +1,6 @@
 import {ProductComponent} from "../../components/product/index.js";
+import {BackButtonComponent} from "../../components/back-button/index.js";
+import {MainPage} from "../main/index.js";
 
 export class ProductPage {
     constructor(parent, id) {
@@ -27,10 +29,20 @@ export class ProductPage {
         )
     }
 
+    clickBack() {
+        const mainPage = new MainPage(this.parent)
+        mainPage.render()
+    }
+
     render() {
+
+        
         this.parent.innerHTML = ''
         const html = this.getHTML()
         this.parent.insertAdjacentHTML('beforeend', html)
+        
+        const backButton = new BackButtonComponent(this.pageRoot)
+        backButton.render(this.clickBack.bind(this))
 
         const data = this.getData()
         const product = new ProductComponent(this.pageRoot)
