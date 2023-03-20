2c24b78e7e17ba5c1a2531a87fa5eb41b4a51e19
korzck <konstantin.koretskiy@gmail.com>
2023-03-20 21:51:39 +0300

    done task 9

--- lab3/components/product-card/index.js
+++ lab3/components/product-card/index.js
@@ -11,25 +11,31 @@ export class ProductCardComponent {
                     <div class="card-body">
                         <h5 class="card-title">${data.title}</h5>
                         <p class="card-text">${data.text}</p>
-                        <button class="btn btn-primary"">Нажми</button>
+                        <button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">Нажми на меня</button>
                     </div>
                 </div>
             `
         )
     }
 
-    getData() {
-        return {
-            id: 1,
-            src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
-            title: "Акция",
-            text: "У меня есть крутая акция"
-        }
+    // getData() {
+    //     return {
+    //         id: 1,
+    //         src: "https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg",
+    //         title: "Акция",
+    //         text: "У меня есть крутая акция"
+    //     }
+    // }
+    addListeners(data, listener) {
+        document
+            .getElementById(`click-card-${data.id}`)
+            .addEventListener("click", listener)
     }
 
-    render(data) {
-        data = this.getData()
-        const html = this.getHTML(data);
-        this.parent.insertAdjacentHTML('beforeend', html);
+    render(data, listener) {
+        // data = this.getData()
+        const html = this.getHTML(data)
+        this.parent.insertAdjacentHTML('beforeend', html)
+        this.addListeners(data, listener)
     }
 }
